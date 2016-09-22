eefec9b5714e423d4f2070ea914c1e24388f74a7
Ryan Miceli <ryan.miceli@gmail.com>
2016-09-22 19:31:16 -0400

    Added support to find hash descriptors and label the vtables.

--- src/Cryptography/LibTomCrypt.py
+++ src/Cryptography/LibTomCrypt.py
@@ -17,15 +17,32 @@ from idc import *
 # Create various Tuples to help us store data.
 AddressableString = namedtuple('AddressableString', 'str, ea')
 
+# Get the list of strings from IDA.
+g_StringList = Strings()
+
+def findString(string, matchExact):
+    # Enumerate through the string list and search for our string.
+    for s in enumerate(g_StringList):
+        # Check if the string is null.
+        if (s is None):
+            pass
+
+        # Check if the current string matches our search criteria.
+        if (matchExact == False and str(s[1]).lower().find(string) != -1):
+            return AddressableString(str(s[1]), s[1].ea)
+        elif (matchExact == True and str(s[1]) == string):
+            return AddressableString(str(s[1]), s[1].ea)
+
+    # The string was not found.
+    return None
+
+
 def buildLTCStringList():
     # Initialize our string list.
     stringList = []
 
-    # Initialize the strings from ida.
-    strings = Strings()
-
     # Loop through all of the strings in the module.
-    for s in enumerate(strings):
+    for s in enumerate(g_StringList):
         # Check if the string is null.
         if (s is None):
             pass
@@ -127,6 +144,72 @@ def labelRSAFunctions(stringList):
     labelFunction(stringList, "rsa_verify_hash.c", "LTC_rsa_verify_hash_ex")
 
 
+def labelHashDescriptor(descName):
+    # Try to find the string label for the hash descriptor.
+    stringAddr = findString(descName, True)
+    if (stringAddr == None):
+        # Failed to find the string label for the hash descriptor.
+        print "LibTomCrypt::labelHashDescriptor(): Failed to find descriptor for '%s', skipping hash..." % descName
+        return
+
+    # Build a list of xrefs to this string.
+    xrefList = XrefsTo(stringAddr.ea, 0)
+    if (xrefList == None):
+        # No xrefs to the descriptor label string.
+        print "LibTomCrypt::labelHashDescriptor(): Failed to find xrefs to string label '%s'!" % descName
+        return
+
+    # Loop through the xref list.
+    for xref in xrefList:
+        # Check to make sure this address is a data address and not a code address.
+        if (xref.iscode != 0):  # 1 is code ref, 0 is data
+            continue
+
+        # Label the descriptor block.
+        idc.MakeName(xref.frm, "LTC_%s_desc" % descName)
+        print "Found hash descriptor for %s" % descName
+
+        # Skip 0x54 bytes ahead and make some dwords which will be the vtable for the descriptor.
+        idc.MakeDword(xref.frm + 0x54)  # init()
+        idc.MakeDword(xref.frm + 0x58)  # process()
+        idc.MakeDword(xref.frm + 0x5C)  # done()
+        idc.MakeDword(xref.frm + 0x60)  # test()
+        idc.MakeDword(xref.frm + 0x64)  # hmac_block()
+
+        # Analyze the init function.
+        initAddr = idc.Dword(xref.frm + 0x54)
+        if (initAddr != 0):
+            idc.MakeFunction(initAddr)
+            set_name(initAddr, "LTC_%s_init" % descName)
+
+        # Analyze the process function.
+        processAddr = idc.Dword(xref.frm + 0x58)
+        if (processAddr != 0):
+            idc.MakeFunction(processAddr)
+            set_name(processAddr, "LTC_%s_process" % descName)
+
+        # Analyze the done function.
+        doneAddr = idc.Dword(xref.frm + 0x5C)
+        if (doneAddr != 0):
+            idc.MakeFunction(doneAddr)
+            set_name(doneAddr, "LTC_%s_done" % descName)
+
+        # Analyze the test function.
+        testAddr = idc.Dword(xref.frm + 0x60)
+        if (testAddr != 0):
+            idc.MakeFunction(testAddr)
+            set_name(testAddr, "LTC_%s_test" % descName)
+
+        # Analyze the hmac_block function.
+        hmacAddr = idc.Dword(xref.frm + 0x64)
+        if (hmacAddr != 0):
+            idc.MakeFunction(hmacAddr)
+            set_name(hmacAddr, "LTC_%s_hmac_block" % descName)
+
+        # We found the hash descriptor so we can break the loop.
+        return
+
+
 def main():
     # Build the list of libtomcrypt strings.
     stringList = buildLTCStringList()
@@ -137,6 +220,37 @@ def main():
 
     # Label the rsa functions.
     labelRSAFunctions(stringList)
+
+    # Label the CHC hash descriptor.
+    labelHashDescriptor("chc_hash")
+
+    # Label the SHA hash descriptors.
+    labelHashDescriptor("sha224")
+    labelHashDescriptor("sha256")
+    labelHashDescriptor("sha384")
+    labelHashDescriptor("sha512_224")
+    labelHashDescriptor("sha512_256")  # We label these two first because they use sha512 functions.
+    labelHashDescriptor("sha512")
+
+    # Label the whirlpool hash descriptor.
+    labelHashDescriptor("whirlpool")
+
+    # Label the MD2/4/5 hash descriptors.
+    labelHashDescriptor("md2")
+    labelHashDescriptor("md4")
+    labelHashDescriptor("md5")
+
+    # Label the RMD128/160/256/320 hash descriptors.
+    labelHashDescriptor("rmd128")
+    labelHashDescriptor("rmd160")
+    labelHashDescriptor("rmd256")
+    labelHashDescriptor("rmd320")
+
+    # Label the SHA1 hash descriptor.
+    labelHashDescriptor("sha1")
+
+    # Label the tiger hash descriptor.
+    labelHashDescriptor("tiger")
 
     # Tell the user we are done.
     print "Done."
