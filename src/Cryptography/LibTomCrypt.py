c73b289395f35b699b6d1409c45339232befa825
Ryan Miceli <ryan.miceli@gmail.com>
2016-09-23 09:00:28 -0400

    Scanning for hash descriptors now verifies the descriptors OID for better reliability in hash descriptor detection.

--- src/Cryptography/LibTomCrypt.py
+++ src/Cryptography/LibTomCrypt.py
@@ -1,5 +1,5 @@
 """
-    Author: Ryan Miceli
+    Author: Ryan Miceli (ryan.miceli@gmail.com)
 
     Created: 9/20/16
 
@@ -16,6 +16,26 @@ from idc import *
 
 # Create various Tuples to help us store data.
 AddressableString = namedtuple('AddressableString', 'str, ea')
+
+# Create our named tuple for the hash descriptors and initialize the descriptors for each hashing algorithm.
+HashDescriptor = namedtuple('HashDescriptor', 'name, id, hashSize, blockSize, oid, oidLength')
+CHC_HASH_DESC           = HashDescriptor("chc_hash", 12, 0, 0, [], 0)
+SHA224_HASH_DESC        = HashDescriptor("sha224", 10, 28, 64, [2, 16, 840, 1, 101, 3, 4, 2, 4], 9)
+SHA256_HASH_DESC        = HashDescriptor("sha256", 0, 32, 64, [2, 16, 840, 1, 101, 3, 4, 2, 1], 9)
+SHA384_HASH_DESC        = HashDescriptor("sha384", 4, 48, 128, [2, 16, 840, 1, 101, 3, 4, 2, 2], 9)
+SHA512_HASH_DESC        = HashDescriptor("sha512", 5, 64, 128, [2, 16, 840, 1, 101, 3, 4, 2, 3], 9)
+SHA512_224_HASH_DESC    = HashDescriptor("sha512-224", 15, 28, 128, [2, 16, 840, 1, 101, 3, 4, 2, 5], 9)
+SHA512_256_HASH_DESC    = HashDescriptor("sha512-256", 16, 32, 128, [2, 16, 840, 1, 101, 3, 4, 2, 6], 9)
+WHIRLPOOL_HASH_DESC     = HashDescriptor("whirlpool", 11, 64, 64, [1, 0, 10118, 3, 0, 55], 6)
+MD2_HASH_DESC           = HashDescriptor("md2", 7, 16, 16, [1, 2, 840, 113549, 2, 2], 6)
+MD4_HASH_DESC           = HashDescriptor("md4", 6, 16, 64, [1, 2, 840, 113549, 2, 4], 6)
+MD5_HASH_DESC           = HashDescriptor("md5", 3, 16, 64, [1, 2, 840, 113549, 2, 5], 6)
+RMD128_HASH_DESC        = HashDescriptor("rmd128", 8, 16, 64, [1, 0, 10118, 3, 0, 50], 6)
+RMD160_HASH_DESC        = HashDescriptor("rmd160", 9, 20, 64, [1, 3, 36, 3, 2, 1], 6)
+RMD256_HASH_DESC        = HashDescriptor("rmd256", 13, 32, 64, [1, 3, 36, 3, 2, 3], 6)
+RMD320_HASH_DESC        = HashDescriptor("rmd320", 14, 40, 64, [], 0)
+SHA1_HASH_DESC          = HashDescriptor("sha1", 2, 20, 64, [1, 3, 14, 3, 2, 26], 6)
+TIGER_HASH_DESC         = HashDescriptor("tiger", 1, 24, 64, [1, 3, 6, 1, 4, 1, 11591, 12, 2], 9)
 
 # Get the list of strings from IDA.
 g_StringList = Strings()
@@ -144,19 +164,19 @@ def labelRSAFunctions(stringList):
     labelFunction(stringList, "rsa_verify_hash.c", "LTC_rsa_verify_hash_ex")
 
 
-def labelHashDescriptor(descName):
+def labelHashDescriptor(hashDesc):
     # Try to find the string label for the hash descriptor.
-    stringAddr = findString(descName, True)
+    stringAddr = findString(hashDesc.name, True)
     if (stringAddr == None):
         # Failed to find the string label for the hash descriptor.
-        print "LibTomCrypt::labelHashDescriptor(): Failed to find descriptor for '%s', skipping hash..." % descName
+        print "LibTomCrypt::labelHashDescriptor(): Failed to find descriptor for '%s', skipping hash..." % hashDesc.name
         return
 
     # Build a list of xrefs to this string.
     xrefList = XrefsTo(stringAddr.ea, 0)
     if (xrefList == None):
         # No xrefs to the descriptor label string.
-        print "LibTomCrypt::labelHashDescriptor(): Failed to find xrefs to string label '%s'!" % descName
+        print "LibTomCrypt::labelHashDescriptor(): Failed to find xrefs to string label '%s'!" % hashDesc.name
         return
 
     # Loop through the xref list.
@@ -165,9 +185,26 @@ def labelHashDescriptor(descName):
         if (xref.iscode != 0):  # 1 is code ref, 0 is data
             continue
 
+        # For better detection we are going to verify the whole hash descriptor.
+        if (idc.Dword(xref.frm + 0x04) != hashDesc.id or idc.Dword(xref.frm + 0x08) != hashDesc.hashSize or
+            idc.Dword(xref.frm + 0x0C) != hashDesc.blockSize):
+            continue
+
+        # Verify the OID matches the descriptor and then we can confirm we are in the right place.
+        valid = True
+        for i in range(0, hashDesc.oidLength):
+            # Check the current OID value.
+            if (idc.Dword(xref.frm + 0x10 + (i * 4)) != hashDesc.oid[i]):
+                valid = False
+                break
+
+        # Check our status bit.
+        if (valid == False):
+            continue
+
         # Label the descriptor block.
-        idc.MakeName(xref.frm, "LTC_%s_desc" % descName)
-        print "Found hash descriptor for %s" % descName
+        idc.MakeName(xref.frm, "LTC_%s_desc" % hashDesc.name)
+        print "Found hash descriptor for %s" % hashDesc.name
 
         # Skip 0x54 bytes ahead and make some dwords which will be the vtable for the descriptor.
         idc.MakeDword(xref.frm + 0x54)  # init()
@@ -180,34 +217,67 @@ def labelHashDescriptor(descName):
         initAddr = idc.Dword(xref.frm + 0x54)
         if (initAddr != 0):
             idc.MakeFunction(initAddr)
-            set_name(initAddr, "LTC_%s_init" % descName)
+            set_name(initAddr, "LTC_%s_init" % hashDesc.name)
 
         # Analyze the process function.
         processAddr = idc.Dword(xref.frm + 0x58)
         if (processAddr != 0):
             idc.MakeFunction(processAddr)
-            set_name(processAddr, "LTC_%s_process" % descName)
+            set_name(processAddr, "LTC_%s_process" % hashDesc.name)
 
         # Analyze the done function.
         doneAddr = idc.Dword(xref.frm + 0x5C)
         if (doneAddr != 0):
             idc.MakeFunction(doneAddr)
-            set_name(doneAddr, "LTC_%s_done" % descName)
+            set_name(doneAddr, "LTC_%s_done" % hashDesc.name)
 
         # Analyze the test function.
         testAddr = idc.Dword(xref.frm + 0x60)
         if (testAddr != 0):
             idc.MakeFunction(testAddr)
-            set_name(testAddr, "LTC_%s_test" % descName)
+            set_name(testAddr, "LTC_%s_test" % hashDesc.name)
 
         # Analyze the hmac_block function.
         hmacAddr = idc.Dword(xref.frm + 0x64)
         if (hmacAddr != 0):
             idc.MakeFunction(hmacAddr)
-            set_name(hmacAddr, "LTC_%s_hmac_block" % descName)
+            set_name(hmacAddr, "LTC_%s_hmac_block" % hashDesc.name)
 
         # We found the hash descriptor so we can break the loop.
         return
+
+
+def labelHashFunctions():
+    # Label the CHC hash descriptor.
+    labelHashDescriptor(CHC_HASH_DESC)
+
+    # Label the SHA hash descriptors.
+    labelHashDescriptor(SHA224_HASH_DESC)
+    labelHashDescriptor(SHA256_HASH_DESC)
+    labelHashDescriptor(SHA384_HASH_DESC)
+    labelHashDescriptor(SHA512_224_HASH_DESC)
+    labelHashDescriptor(SHA512_256_HASH_DESC)  # We label these two first because they use sha512 functions.
+    labelHashDescriptor(SHA512_HASH_DESC)
+
+    # Label the whirlpool hash descriptor.
+    labelHashDescriptor(WHIRLPOOL_HASH_DESC)
+
+    # Label the MD2/4/5 hash descriptors.
+    labelHashDescriptor(MD2_HASH_DESC)
+    labelHashDescriptor(MD4_HASH_DESC)
+    labelHashDescriptor(MD5_HASH_DESC)
+
+    # Label the RMD128/160/256/320 hash descriptors.
+    labelHashDescriptor(RMD128_HASH_DESC)
+    labelHashDescriptor(RMD160_HASH_DESC)
+    labelHashDescriptor(RMD256_HASH_DESC)
+    labelHashDescriptor(RMD320_HASH_DESC)
+
+    # Label the SHA1 hash descriptor.
+    labelHashDescriptor(SHA1_HASH_DESC)
+
+    # Label the tiger hash descriptor.
+    labelHashDescriptor(TIGER_HASH_DESC)
 
 
 def main():
@@ -221,36 +291,8 @@ def main():
     # Label the rsa functions.
     labelRSAFunctions(stringList)
 
-    # Label the CHC hash descriptor.
-    labelHashDescriptor("chc_hash")
-
-    # Label the SHA hash descriptors.
-    labelHashDescriptor("sha224")
-    labelHashDescriptor("sha256")
-    labelHashDescriptor("sha384")
-    labelHashDescriptor("sha512_224")
-    labelHashDescriptor("sha512_256")  # We label these two first because they use sha512 functions.
-    labelHashDescriptor("sha512")
-
-    # Label the whirlpool hash descriptor.
-    labelHashDescriptor("whirlpool")
-
-    # Label the MD2/4/5 hash descriptors.
-    labelHashDescriptor("md2")
-    labelHashDescriptor("md4")
-    labelHashDescriptor("md5")
-
-    # Label the RMD128/160/256/320 hash descriptors.
-    labelHashDescriptor("rmd128")
-    labelHashDescriptor("rmd160")
-    labelHashDescriptor("rmd256")
-    labelHashDescriptor("rmd320")
-
-    # Label the SHA1 hash descriptor.
-    labelHashDescriptor("sha1")
-
-    # Label the tiger hash descriptor.
-    labelHashDescriptor("tiger")
+    # Label the hash descriptors.
+    labelHashFunctions()
 
     # Tell the user we are done.
     print "Done."
